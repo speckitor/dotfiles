01c720d0c52328f7cb62e49daccb5ba533220d9c
Speckitor <pasaloginov381@gmail.com>
2025-05-11 23:19:24 +0700

    upd

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -19,6 +19,10 @@ vim.opt.smartcase = true
 
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
+vim.o.expandtab = true
+
+vim.o.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
 vim.opt.updatetime = 250
 vim.opt.timeoutlen = 300
@@ -69,154 +73,154 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	{
-		"rebelot/kanagawa.nvim",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			vim.cmd.colorscheme("kanagawa")
-		end
-	},
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		config = function()
-			  local configs = require("nvim-treesitter.configs")
-			  configs.setup({
-				  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "python" },
-				  sync_install = false,
-				  highlight = { enable = true },
-				  indent = { enable = true },
-			})
-		end
-	},
-	{
-		'echasnovski/mini.nvim',
-		config = function()
-			require('mini.ai').setup()
-			require('mini.pairs').setup()
-			require('mini.surround').setup()
-			require('mini.statusline').setup()
-		end,
-	},
-	{
-		'nvim-telescope/telescope.nvim',
-		event = 'VimEnter',
-		branch = '0.1.x',
-		dependencies = {
-			'nvim-lua/plenary.nvim',
-		},
-		config = function()
-			require('telescope').setup()
-			pcall(require('telescope').load_extension, 'fzf')
+    {
+        "webhooked/kanso.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            vim.cmd.colorscheme("kanso-zen")
+        end
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+              local configs = require("nvim-treesitter.configs")
+              configs.setup({
+                  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "python" },
+                  sync_install = false,
+                  highlight = { enable = true },
+                  indent = { enable = true },
+            })
+        end
+    },
+    {
+        'echasnovski/mini.nvim',
+        config = function()
+            require('mini.ai').setup()
+            require('mini.pairs').setup()
+            require('mini.surround').setup()
+            require('mini.statusline').setup()
+        end,
+    },
+    {
+        'nvim-telescope/telescope.nvim',
+        event = 'VimEnter',
+        branch = '0.1.x',
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+        },
+        config = function()
+            require('telescope').setup()
+            pcall(require('telescope').load_extension, 'fzf')
 
-			local builtin = require 'telescope.builtin'
-			vim.keymap.set('n', '<leader>sf', builtin.find_files)
-			vim.keymap.set('n', '<leader>sg', builtin.live_grep)
-			vim.keymap.set('n', '<leader>sd', builtin.diagnostics)
-			vim.keymap.set('n', '<leader>sb', builtin.buffers)
-		end,
-	},
-	{
-		'neovim/nvim-lspconfig',
-		dependencies = {
-			"williamboman/mason.nvim",
-			"williamboman/mason-lspconfig.nvim",
-			"hrsh7th/cmp-nvim-lsp",
-		},
-		config = function()
-			require("mason").setup()
-			require("mason-lspconfig").setup {
-				ensure_installed = {
-					"lua_ls",
-					"rust_analyzer",
-					"clangd",
-					"pyright",
-				},
-			}
+            local builtin = require 'telescope.builtin'
+            vim.keymap.set('n', '<leader>sf', builtin.find_files)
+            vim.keymap.set('n', '<leader>sg', builtin.live_grep)
+            vim.keymap.set('n', '<leader>sd', builtin.diagnostics)
+            vim.keymap.set('n', '<leader>sb', builtin.buffers)
+        end,
+    },
+    {
+        'neovim/nvim-lspconfig',
+        dependencies = {
+            "williamboman/mason.nvim",
+            "williamboman/mason-lspconfig.nvim",
+            "hrsh7th/cmp-nvim-lsp",
+        },
+        config = function()
+            require("mason").setup()
+            require("mason-lspconfig").setup {
+                ensure_installed = {
+                    "lua_ls",
+                    "rust_analyzer",
+                    "clangd",
+                    "pyright",
+                },
+            }
 
-			vim.api.nvim_create_autocmd('LspAttach', {
-			  desc = 'LSP actions',
-			  callback = function(event)
-				local opts = {buffer = event.buf}
+            vim.api.nvim_create_autocmd('LspAttach', {
+              desc = 'LSP actions',
+              callback = function(event)
+                local opts = {buffer = event.buf}
 
-				vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-				vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-				vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-				vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-				vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-				vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-				vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-				vim.keymap.set('n', 'ca', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
-			  end,
-			})
+                vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+                vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+                vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+                vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+                vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+                vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+                vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+                vim.keymap.set('n', 'ca', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+              end,
+            })
 
-		    local capabilities = require('cmp_nvim_lsp').default_capabilities()
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-			require("lspconfig").lua_ls.setup({capabilities = capabilities})
-			require("lspconfig").rust_analyzer.setup({capabilities = capabilities})
-			require("lspconfig").clangd.setup({
-				capabilities = capabilities,
-				cmd = {"clangd", "--header-insertion=never"}
-			})
-			require("lspconfig").pyright.setup({capabilities = capabilities})
-		end
-	},
-	{
-		'hrsh7th/nvim-cmp',
-		event = 'InsertEnter',
-		dependencies = {
-			{
-				'L3MON4D3/LuaSnip',
-				build = (function()
-					if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-						return
-					end
-					return 'make install_jsregexp'
-				end)(),
-				dependencies = {},
-			},
-			'saadparwaiz1/cmp_luasnip',
-			'hrsh7th/cmp-nvim-lsp',
-			'hrsh7th/cmp-buffer',
-			'hrsh7th/cmp-path',
-			'hrsh7th/cmp-nvim-lsp-signature-help',
-		},
-		config = function()
-			local cmp = require 'cmp'
-			local luasnip = require 'luasnip'
-      		luasnip.config.setup {}
+            require("lspconfig").lua_ls.setup({capabilities = capabilities})
+            require("lspconfig").rust_analyzer.setup({capabilities = capabilities})
+            require("lspconfig").clangd.setup({
+                capabilities = capabilities,
+                cmd = {"clangd", "--header-insertion=never"}
+            })
+            require("lspconfig").pyright.setup({capabilities = capabilities})
+        end
+    },
+    {
+        'hrsh7th/nvim-cmp',
+        event = 'InsertEnter',
+        dependencies = {
+            {
+                'L3MON4D3/LuaSnip',
+                build = (function()
+                    if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
+                        return
+                    end
+                    return 'make install_jsregexp'
+                end)(),
+                dependencies = {},
+            },
+            'saadparwaiz1/cmp_luasnip',
+            'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-buffer',
+            'hrsh7th/cmp-path',
+            'hrsh7th/cmp-nvim-lsp-signature-help',
+        },
+        config = function()
+            local cmp = require 'cmp'
+            local luasnip = require 'luasnip'
+            luasnip.config.setup {}
 
-			cmp.setup {
-				snippet = {
-				expand = function(args)
-					luasnip.lsp_expand(args.body)
-				end,
-				},
-				completion = { completeopt = 'menu,menuone,noinsert' },
-				mapping = cmp.mapping.preset.insert {
-					['<C-n>'] = cmp.mapping.select_next_item(),
-					['<C-p>'] = cmp.mapping.select_prev_item(),
-					['<C-b>'] = cmp.mapping.scroll_docs(-4),
-					['<C-f>'] = cmp.mapping.scroll_docs(4),
-					['<C-y>'] = cmp.mapping.confirm { select = true },
-					['<S-Tab>'] = cmp.mapping.complete {},
-				},
-				sources = {
-					{ name = 'nvim_lsp' },
-					{ name = 'luasnip' },
-					{ name = 'path' },
-					{ name = 'buffer' },
-					{ name = 'nvim_lsp_signature_help' },
-				},
-			}
-		end,
-	},
-	{
-		'santigo-zero/right-corner-diagnostics.nvim',
-		event = 'LspAttach',
-		config = function ()
-			require('rcd').setup()
-		end
-	}
+            cmp.setup {
+                snippet = {
+                expand = function(args)
+                    luasnip.lsp_expand(args.body)
+                end,
+                },
+                completion = { completeopt = 'menu,menuone,noinsert' },
+                mapping = cmp.mapping.preset.insert {
+                    ['<C-n>'] = cmp.mapping.select_next_item(),
+                    ['<C-p>'] = cmp.mapping.select_prev_item(),
+                    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                    ['<C-y>'] = cmp.mapping.confirm { select = true },
+                    ['<S-Tab>'] = cmp.mapping.complete {},
+                },
+                sources = {
+                    { name = 'nvim_lsp' },
+                    { name = 'luasnip' },
+                    { name = 'path' },
+                    { name = 'buffer' },
+                    { name = 'nvim_lsp_signature_help' },
+                },
+            }
+        end,
+    },
+    {
+        'santigo-zero/right-corner-diagnostics.nvim',
+        event = 'LspAttach',
+        config = function ()
+            require('rcd').setup()
+        end
+    }
 })
