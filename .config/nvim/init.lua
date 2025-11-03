93735f2276c35c1b35b35d826d6aa5212d07f999
speckitor <pasaloginov381@gmail.com>
2025-11-04 00:18:32 +0700

    upd

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,7 @@
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.cursorline = true
+vim.o.winborder = "rounded"
 vim.o.incsearch = true
 vim.o.ignorecase = true
 vim.o.smartcase = true
@@ -21,7 +22,94 @@ vim.keymap.set({ "n", "v", "x" }, ";", ":")
 vim.keymap.set({ "n", "v", "x" }, ":", ";")
 
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
-vim.keymap.set("n", "<leader>f", "<cmd>Ex<CR>")
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
 
-vim.cmd("colorscheme wildcharm")
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out, "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+    {
+        "scottmckendry/cyberdream.nvim",
+        priority = 1000,
+        config = function()
+            vim.cmd("colorscheme cyberdream")
+        end,
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            local configs = require("nvim-treesitter.configs")
+
+            configs.setup({
+                ensure_installed = { "lua", "c", "cpp", "rust", "python" },
+                sync_install = false,
+                highlight = { enable = true },
+                indent = { enable = true },
+            })
+        end,
+    },
+    { 'nvim-mini/mini.comment', version = false },
+    {
+        "nvim-telescope/telescope.nvim", tag = "0.1.8",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function ()
+            local builtin = require("telescope.builtin")
+            vim.keymap.set("n", "<leader>f", builtin.find_files)
+            vim.keymap.set("n", "<leader>g", builtin.live_grep)
+            vim.keymap.set("n", "<leader>b", builtin.buffers)
+        end
+    },
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            "williamboman/mason.nvim",
+        },
+        config = function()
+            require("mason").setup()
+
+            vim.keymap.set("n", "K", vim.lsp.buf.hover)
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+            vim.keymap.set("n", "gD", vim.lsp.buf.declaration)
+            vim.keymap.set("n", "gi", vim.lsp.buf.implementation)
+            vim.keymap.set("n", "gt", vim.lsp.buf.type_definition)
+            vim.keymap.set("n", "gr", vim.lsp.buf.references)
+            vim.keymap.set("n", "ca", vim.lsp.buf.code_action)
+            vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float)
+            vim.keymap.set("n", '<leader>D', vim.diagnostic.setloclist)
+
+            vim.lsp.enable({
+                "lua_ls",
+                "clangd",
+                "rust_analyzer",
+                "pyright",
+                "glsl_analyzer",
+            })
+        end,
+    },
+    {
+        "saghen/blink.cmp",
+        dependencies = { "rafamadriz/friendly-snippets" },
+        version = "1.*",
+        opts = {
+            keymap = { preset = "default" },
+            appearance = { nerd_font_variant = "mono" },
+            completion = { documentation = { auto_show = false } },
+            fuzzy = { implementation = "prefer_rust_with_warning" },
+        },
+        opts_extend = { "sources.default" }
+    },
+})
